96e15f230ab7ec9676baf06231397d48fdcb33fb
Zack Spring Chief <jzacksc@gmail.com>
2024-02-08 14:34:45 -0700

    creating new user with model binding

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using coreFormsAndValidations.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AccountsViewController;
@@ -7,5 +8,15 @@ public class AccountController : Controller
     public IActionResult StronglyTypedLogin()
     {
         return View();
+    }
+    [HttpPost]
+    public IActionResult LoginSuccess()
+    {
+        return View();
+    }
+    public IActionResult UserDetail()
+    {
+        var user = new LoginViewModel() { Username = "Zack", Password = "pass1234" };
+        return View(user);
     }
 }
