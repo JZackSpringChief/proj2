0ed61b5c763a36a4a09b5b04283fc5562d62d78e
Zack Spring Chief <jzacksc@gmail.com>
2024-02-08 15:14:39 -0700

    adding account model

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -19,4 +19,17 @@ public class AccountController : Controller
         var user = new LoginViewModel() { Username = "Zack", Password = "pass1234" };
         return View(user);
     }
+    public IActionResult GetAccout()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult PostAccount(Account account)
+    {
+        if (ModelState.IsValid)
+        {
+            return View("Success");
+        }
+        return RedirectToAction("GetAccount");
+    }
 }
